9aa275dce3fc0044f0bb8ae9a79cb7446c4834af
MistyBlunch <gracenikole@gmail.com>
2021-06-19 16:14:07 -0500

    sis

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let UIkit: any;
+
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.sass']
 })
 export class HeroComponent implements OnInit {
-
+  
   constructor() { }
-
+  
   ngOnInit(): void {
+    
   }
 
 }
